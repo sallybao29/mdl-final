ec595275692a364abb3a1b628736c552d77b5f72
Sandy Fang <sfang2016@gmail.com>
2016-06-12 15:50:27 -0400

    spaghetti MESHES WOrk

--- my_main.c
+++ my_main.c
@@ -254,8 +254,8 @@ void my_main( int polygons ) {
   char frame_name[128];
 
   FILE *mesh_f;
-  char mesh_n[255];
-  char mesh_l[255];
+  char mesh_n[256];
+  char mesh_l[256];
   struct matrix *mesh_m;
   double v1; double v2; double v3;
   int fi1; int fi2; int fi3;
@@ -366,17 +366,29 @@ void my_main( int polygons ) {
         mesh_m = new_matrix(4,1000);
         strcpy(mesh_n,op[i].op.mesh.name);
         mesh_f = fopen(mesh_n, "r");
-        while( fgets(mesh_l, 255, mesh_f) != NULL ){
-          if( fgetc(mesh_f) == 'v' ){
-            fseek(mesh_f,-1,SEEK_CUR);
+        while( /*!feof(mesh_f)*/ fgets(mesh_l, 255, mesh_f) != NULL ){
+
+          /*
+          fseek(mesh_f,-1,SEEK_CUR);
+          fgets(mesh_l, 255, mesh_f);
+          */
+          mesh_l[strlen(mesh_l)-1]='\0';
+          //printf("READ LINE: [%s]\n", mesh_l);
+
+          if( mesh_l[0] == 'v' ){
+            //fseek(mesh_f,-1,SEEK_CUR);
             sscanf(mesh_l, "v %lf %lf %lf\n", &v1, &v2, &v3);
             add_point(mesh_m, v1 , v2, v3);
             //printf("read: %lf %lf %lf\n",v1, v2, v3);
           }
-          if( fgetc(mesh_f) == 'f' ){
-            fseek(mesh_f,-1,SEEK_CUR);
+          if( mesh_l[0] == 'f' ){
+            //fseek(mesh_f,-1,SEEK_CUR);
             sscanf(mesh_l, "f %d %d %d\n", &fi1, &fi2, &fi3);
-            printf("DEBUGGING %lf %lf %lf\n",mesh_m->m[0][fi1], mesh_m->m[1][fi1], mesh_m->m[2][fi1] );
+            fi1--; //THE MESH FILE DOESN'T HAVE ZERO BASED INDEXING GRRRRR
+            fi2--;
+            fi3--;
+            //printf("DEBUGGING INDEX %d %d %d\n", fi1, fi2, fi3);
+            //printf("DEBUGGING VERTICES %lf %lf %lf\n",mesh_m->m[0][fi1], mesh_m->m[1][fi1], mesh_m->m[2][fi1] );
             add_polygon(tmp, mesh_m->m[0][fi1], mesh_m->m[1][fi1], mesh_m->m[2][fi1],
                       mesh_m->m[0][fi2],mesh_m->m[1][fi2],mesh_m->m[2][fi2],
                       mesh_m->m[0][fi3],mesh_m->m[1][fi3],mesh_m->m[2][fi3]);
@@ -485,7 +497,7 @@ void my_main( int polygons ) {
 
   free_stack( s );
   free_matrix( tmp );
-  free_matrix( mesh_m );
+  //free_matrix( mesh_m );
   if (num_frames > 1){
     for (i = 0; i < num_frames; i++){
       while (knobs[i] != NULL){
